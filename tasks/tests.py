5dc4c5d71cc320654b7280986f38ec16d8e88a52
MullakhmetovDanil <danilmullakhmetov@gmail.com>
2025-04-01 14:41:32 +0500

    add CRUD test for tasks

--- tasks/tests.py
+++ tasks/tests.py
@@ -1,3 +1,80 @@
 from django.test import TestCase
+from django.urls import reverse
+from django.contrib.auth.models import User
+from tasks.models import Task
+from statuses.models import Status
+from django.contrib.auth import get_user_model
 
-# Create your tests here.
+User = get_user_model()
+
+class TaskTests(TestCase):
+
+    def setUp(self):
+        self.user = User.objects.create_user(username='testuser', password='testpassword')
+        self.another_user = User.objects.create_user(username='anotheruser', password='testpassword')
+
+        self.status = Status.objects.create(name='In Progress')
+
+        self.task = Task.objects.create(
+            name="Test Task",
+            description="Test Task Description",
+            executor=self.user,
+            status=self.status,
+            author=self.user
+        )
+
+        self.client.login(username='testuser', password='testpassword')
+
+    def test_task_list_view(self):
+        response = self.client.get(reverse('tasks_list'))
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'tasks/task_list.html')
+        self.assertContains(response, 'Test Task')
+
+    def test_task_create_view(self):
+        response = self.client.post(reverse('task_form'), {
+            'name': 'New Task',
+            'description': 'New Task Description',
+            'executor': self.user.id,
+            'status': self.status.id,
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('tasks_list'))
+        self.assertTrue(Task.objects.filter(name='New Task').exists())
+
+    def test_task_update_view(self):
+        response = self.client.post(reverse('task_update', args=[self.task.id]), {
+            'name': 'Updated Task',
+            'description': 'Updated Description',
+            'executor': self.user.id,
+            'status': self.status.id,
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('tasks_list'))
+        self.task.refresh_from_db()
+        self.assertEqual(self.task.name, 'Updated Task')
+
+    def test_task_delete_view(self):
+        response = self.client.post(reverse('task_delete', args=[self.task.id]))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('tasks_list'))
+        self.assertFalse(Task.objects.filter(id=self.task.id).exists())
+
+    def test_task_delete_view_permission(self):
+        self.client.logout()
+        self.client.login(username='anotheruser', password='testpassword')
+        response = self.client.post(reverse('task_delete', args=[self.task.id]))
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, reverse('tasks_list'))
+        self.assertTrue(Task.objects.filter(id=self.task.id).exists())
+
+    def test_task_create_view_not_authenticated(self):
+        self.client.logout()
+        response = self.client.post(reverse('task_form'), {
+            'name': 'New Task',
+            'description': 'New Task Description',
+            'executor': self.user.id,
+            'status': self.status.id,
+        })
+        self.assertEqual(response.status_code, 302)
+        self.assertRedirects(response, '/login/?next=/tasks/create/')
--- tasks/views.py
+++ tasks/views.py
@@ -5,11 +5,41 @@ from tasks.models import Task
 from django.shortcuts import render
 from user.models import User
 from statuses.models import Status
+from django.contrib import messages
+from django.shortcuts import redirect
 
 class TasksView(LoginRequiredMixin, ListView):
     model = Task
     template_name = 'tasks/tasks_list.html'
     context_object_name = 'tasks'
+
+    def get_queryset(self):
+        queryset = Task.objects.all()
+        status_id = self.request.GET.get('status')
+        executor_id = self.request.GET.get('executor')
+        self_tasks = self.request.GET.get('self_tasks') == "on"
+
+        print(f"self_tasks: {self_tasks}, user: {self.request.user}")
+
+        if status_id:
+            queryset = queryset.filter(status_id=status_id)
+        if executor_id:
+            queryset = queryset.filter(executor_id=executor_id)
+        if self_tasks and self.request.user.is_authenticated:
+            queryset = queryset.filter(author_id=self.request.user.id)
+
+        print(queryset.query)
+        print(list(queryset))
+
+        return queryset
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['statuses'] = Status.objects.all()
+        context['users'] = User.objects.all()
+        return context
+
+
 
 class TaskView(LoginRequiredMixin, DetailView):
     model = Task
@@ -26,15 +56,17 @@ class TaskCreateView(LoginRequiredMixin, CreateView):
         form.instance.author = self.request.user
         return super().form_valid(form)
 
-class TaskUpdateView(LoginRequiredMixin, UserPassesTestMixin, UpdateView):
+class TaskUpdateView(LoginRequiredMixin, UpdateView):
     model = Task
     template_name = 'tasks/task_form.html'
     fields = ['name', 'description', 'executor', 'status']
     success_url = reverse_lazy('tasks_list')
 
-    def test_func(self):
-        task = self.get_object()
-        return self.request.user == task.author
+    def form_valid(self, form):
+        messages.success(self.request, "Задача успешно обновлена.")
+        return super().form_valid(form)
+
+
 
 class TaskDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     model = Task
@@ -44,3 +76,7 @@ class TaskDeleteView(LoginRequiredMixin, UserPassesTestMixin, DeleteView):
     def test_func(self):
         task = self.get_object()
         return self.request.user == task.author
+    
+    def handle_no_permission(self):
+        messages.error(self.request, "Задачу может удалить только ее автор.")
+        return redirect('tasks_list')
