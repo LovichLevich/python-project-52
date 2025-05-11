7611e7e67405c2af27466a809d3cec4fd84b934e
MullakhmetovDanil <danilmullakhmetov@gmail.com>
2025-05-11 22:12:53 +0300

    Refactor label create and update views to use generic class-based views

--- task_manager/apps/labels/views.py
+++ task_manager/apps/labels/views.py
@@ -5,6 +5,7 @@ from django.urls import reverse_lazy
 from django.utils.decorators import method_decorator
 from django.utils.translation import gettext_lazy as _
 from django.views import View
+from django.views.generic.edit import CreateView, UpdateView
 
 from task_manager.forms import LabelForm
 
@@ -19,37 +20,36 @@ class LabelListView(View):
 
 
 @method_decorator(login_required, name="dispatch")
-class LabelCreateView(View):
-    def get(self, request):
-        form = LabelForm()
-        return render(request, 'labels/label_form.html', {'form': form})
+class LabelCreateView(CreateView):
+    model = Labels
+    form_class = LabelForm
+    template_name = 'labels/label_form.html'
+    success_url = reverse_lazy('label_list')
 
-    def post(self, request):
-        form = LabelForm(request.POST)
-        if form.is_valid():
-            label = form.save(commit=False)
-            label.author = request.user
-            label.save()
-            messages.success(request, _("The label was created successfully."))
-            return redirect(reverse_lazy('label_list'))
-        return render(request, 'labels/label_form.html', {'form': form})
+    def form_valid(self, form):
+        form.instance.author = self.request.user
+        messages.success(self.request, _("The label was created successfully."))
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, _("Label creation error. Check the data"))
+        return super().form_invalid(form)
 
 
 @method_decorator(login_required, name="dispatch")
-class LabelUpdateView(View):
-    def get(self, request, pk):
-        label = get_object_or_404(Labels, pk=pk)
-        form = LabelForm(instance=label)
-        return render(request, 'labels/label_form.html', {'form': form})
+class LabelUpdateView(UpdateView):
+    model = Labels
+    form_class = LabelForm
+    template_name = 'labels/label_form.html'
+    success_url = reverse_lazy('label_list')
 
-    def post(self, request, pk):
-        label = get_object_or_404(Labels, pk=pk)
-        form = LabelForm(request.POST, instance=label)
-        if form.is_valid():
-            form.save()
-            messages.success(request, _("Label is successfully changed"))
-            return redirect(reverse_lazy('label_list'))
-        return render(request, 'labels/label_form.html', {'form': form})
+    def form_valid(self, form):
+        messages.success(self.request, _("Label is successfully changed"))
+        return super().form_valid(form)
+
+    def form_invalid(self, form):
+        messages.error(self.request, _("Label update error. Check the data"))
+        return super().form_invalid(form)
 
 
 @method_decorator(login_required, name="dispatch")
